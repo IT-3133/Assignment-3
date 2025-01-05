dee9bb98cbee4c410b05c5784f7b3f274b41a2bc
Thilankavishka <thilankavishka25@gmail.com>
2025-01-05 11:57:25 +0530

    Make component structure with Login  Component

--- studentcare/App.js
+++ studentcare/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./components/Login";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>UoV Student Care</Text>
+      </View>
+      <View>
+        <Image
+          source={require("./assets/uovlogo.png")}
+          styles={styles.image}
+        ></Image>
+      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="login"
+          screenOptions={{ headerStyle: { backgroundColor: "e2bee2" } }}
+        >
+          <Stack.Screen
+            name="login"
+            component={Login}
+            //options={{ title: "UoV Student Care" }}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    width: "100%",
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8b008b",
+  },
+  headerText: {
+    fontSize: 24,
+    color: "#fff",
+    backgroundColor: "#8b008b",
+    fontWeight: "bold",
+  },
+  image: {
+    width: "85%",
+    height: 80,
   },
 });
--- studentcare/components/Login.js
+++ studentcare/components/Login.js
@@ -1,19 +1,24 @@
+import { Image, ScrollView, StyleSheet } from "react-native";
+import { Divider, PaperProvider, Text } from "react-native-paper";
+import { View } from "react-native-web";
+
 export default function Login() {
-
-
-return (
-
-    
-   
-
-
-
-
-
-)
-
-
-
-
-
+  return (
+    <PaperProvider>
+      <ScrollView>
+        <View style={Styles.container}>
+          <Text>Student Login</Text>
+          <Divider />
+        </View>
+      </ScrollView>
+    </PaperProvider>
+  );
 }
+
+const Styles = StyleSheet.create({
+  container: {},
+  image: {
+    width: "85%",
+    height: 80,
+  },
+});
